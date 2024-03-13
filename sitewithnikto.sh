1f0716bdc8e1b5744c2f8e15c823b4825bcebd0d
Jigar R Rathod <136077334+jrrathod1505@users.noreply.github.com>
2024-03-13 18:23:55 +0530

    Update sitewithnikto.sh

--- sitewithnikto.sh
+++ sitewithnikto.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 # Check if the target URL is provided as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <target URL>"
-    exit 1
-fi
 echo "Enter the target Website : "
 read target_url
 # Check if nikto is installed
